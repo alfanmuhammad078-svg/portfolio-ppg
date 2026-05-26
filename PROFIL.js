8c866965582e0262dd7961958b75958b74db7e23
alfanmuhammad078-svg <alfanmuhammad078@gmail.com>
2026-05-26 08:18:57 +0700

    Add files via upload

---
+++ PROFIL.js
@@ -0,0 +1,8 @@
+function showMore() {
+  var detail = document.getElementById("detail");
+  if (detail.style.display === "none") {
+    detail.style.display = "block";
+  } else {
+    detail.style.display = "none";
+  }
+} 
